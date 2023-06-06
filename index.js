16041fc7a84c83a7ea155a451925a2202e60bc72
Ganeshramar <ganeshramar98@gmail.com>
2023-06-06 13:27:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var button = document.querySelector('#start');
+var container = document.querySelector('#jitsi-container');
+var api = null;
+
+button.addEventListener('click', () => {
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var stringLength = 30;
+
+    function pickRandom() {
+    return possible[Math.floor(Math.random() * possible.length)];
+    }
+
+var randomString = Array.apply(null, Array(stringLength)).map(pickRandom).join('');
+
+    var domain = "meet.jit.si";
+    var options = {
+        "roomName": randomString,
+        "parentNode": container,
+        // "width": 600,
+        // "height": 600,
+    };
+    api = new JitsiMeetExternalAPI(domain, options);
+});
+
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+// function startVideo()
+// {
+// 	document.location.href = "index.html";
+// }
